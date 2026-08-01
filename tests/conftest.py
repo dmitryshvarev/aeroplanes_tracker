9eb83b97147f7e7c8f781cd1f0f09f09903f097e
Dmitry Shvarev <dima.dreadman@mail.ru>
2026-08-02 03:20:30 +0500

    add test_aeroplane.py

---
+++ tests/conftest.py
@@ -0,0 +1,97 @@
+"""Фикстуры для тестирования."""
+
+import json
+import tempfile
+from typing import Any, Dict, List
+
+import pytest
+
+from src.aeroplane import Aeroplane
+
+
+@pytest.fixture
+def sample_aeroplanes_data() -> List[Dict[str, Any]]:
+    """Фикстура с тестовыми данными самолетов."""
+    return [
+        {
+            'callsign': 'AFL101',
+            'origin_country': 'Russia',
+            'velocity': 250.5,
+            'altitude': 10500.0,
+            'latitude': 55.5,
+            'longitude': 37.5,
+            'icao24': 'abc123'
+        },
+        {
+            'callsign': 'BAW202',
+            'origin_country': 'United Kingdom',
+            'velocity': 240.0,
+            'altitude': 9500.0,
+            'latitude': 51.5,
+            'longitude': -0.1,
+            'icao24': 'def456'
+        },
+        {
+            'callsign': 'UAL303',
+            'origin_country': 'United States',
+            'velocity': 260.0,
+            'altitude': 11000.0,
+            'latitude': 40.7,
+            'longitude': -74.0,
+            'icao24': 'ghi789'
+        },
+        {
+            'callsign': None,
+            'origin_country': 'Germany',
+            'velocity': None,
+            'altitude': None,
+            'latitude': 52.5,
+            'longitude': 13.4,
+            'icao24': 'jkl012'
+        }
+    ]
+
+
+@pytest.fixture
+def sample_aeroplanes_objects(sample_aeroplanes_data) -> List[Aeroplane]:
+    """Фикстура с тестовыми объектами Aeroplane."""
+    return [Aeroplane.from_dict(data) for data in sample_aeroplanes_data]
+
+
+@pytest.fixture
+def temp_json_file():
+    """Фикстура для временного JSON файла."""
+    with tempfile.NamedTemporaryFile(mode='w', suffix='.json', delete=False) as f:
+        json.dump([], f)
+        temp_file = f.name
+
+    yield temp_file
+
+    import os
+    os.unlink(temp_file)
+
+
+@pytest.fixture
+def mock_nominatim_response():
+    """Фикстура с мок-ответом от Nominatim API."""
+    return [
+        {
+            'place_id': 123,
+            'boundingbox': ['35.0', '70.0', '-10.0', '40.0'],
+            'display_name': 'Russia'
+        }
+    ]
+
+
+@pytest.fixture
+def mock_opensky_response():
+    """Фикстура с мок-ответом от OpenSky API."""
+    return {
+        'time': 1234567890,
+        'states': [
+            ['abc123', 'AFL101  ', 'Russia', 1234567890, 1234567890,
+             37.5, 55.5, 10500.0, False, 250.5, 120.0, 0.0, None, 11000.0, None, False, 0],
+            ['def456', 'BAW202', 'United Kingdom', 1234567890, 1234567890,
+             -0.1, 51.5, 9500.0, False, 240.0, 110.0, 0.0, None, 10000.0, None, False, 0],
+        ]
+    }
---
+++ tests/test_aeroplane.py
@@ -0,0 +1,160 @@
+"""Тесты для класса Aeroplane."""
+
+import pytest
+
+from src.aeroplane import Aeroplane
+from src.exceptions import InvalidAeroplaneDataError
+
+
+class TestAeroplane:
+    """Тесты для класса Aeroplane."""
+
+    def test_aeroplane_creation_valid(self, sample_aeroplanes_data):
+        """Тест создания самолета с валидными данными."""
+        data = sample_aeroplanes_data[0]
+        aeroplane = Aeroplane(
+            callsign=data['callsign'],
+            origin_country=data['origin_country'],
+            velocity=data['velocity'],
+            altitude=data['altitude'],
+            latitude=data['latitude'],
+            longitude=data['longitude'],
+            icao24=data['icao24']
+        )
+
+        assert aeroplane.callsign == 'AFL101'
+        assert aeroplane.origin_country == 'Russia'
+        assert aeroplane.velocity == 250.5
+        assert aeroplane.altitude == 10500.0
+        assert aeroplane.latitude == 55.5
+        assert aeroplane.longitude == 37.5
+        assert aeroplane.icao24 == 'abc123'
+
+    def test_aeroplane_creation_with_none_values(self):
+        """Тест создания самолета с None значениями."""
+        aeroplane = Aeroplane(
+            callsign=None,
+            origin_country='Russia',
+            velocity=None,
+            altitude=None
+        )
+
+        assert aeroplane.callsign is None
+        assert aeroplane.origin_country == 'Russia'
+        assert aeroplane.velocity is None
+        assert aeroplane.altitude is None
+
+    def test_aeroplane_creation_invalid_callsign(self):
+        """Тест создания с невалидным позывным."""
+        with pytest.raises(InvalidAeroplaneDataError):
+            Aeroplane(
+                callsign=123,  # Должна быть строка
+                origin_country='Russia',
+                velocity=100.0,
+                altitude=10000.0
+            )
+
+    def test_aeroplane_creation_invalid_country(self):
+        """Тест создания с невалидной страной."""
+        with pytest.raises(InvalidAeroplaneDataError):
+            Aeroplane(
+                callsign='TEST',
+                origin_country='',  # Пустая строка
+                velocity=100.0,
+                altitude=10000.0
+            )
+
+    def test_aeroplane_creation_invalid_velocity(self):
+        """Тест создания с невалидной скоростью."""
+        with pytest.raises(InvalidAeroplaneDataError):
+            Aeroplane(
+                callsign='TEST',
+                origin_country='Russia',
+                velocity='not a number',
+                altitude=10000.0
+            )
+
+    def test_aeroplane_creation_negative_velocity(self):
+        """Тест создания с отрицательной скоростью."""
+        with pytest.raises(InvalidAeroplaneDataError):
+            Aeroplane(
+                callsign='TEST',
+                origin_country='Russia',
+                velocity=-100.0,
+                altitude=10000.0
+            )
+
+    def test_aeroplanes_comparison_lt(self, sample_aeroplanes_objects):
+        """Тест сравнения 'меньше' по высоте."""
+        a1, a2, a3 = sample_aeroplanes_objects[:3]
+
+        # a2 (9500) < a1 (10500)
+        assert a2 < a1
+        assert not (a1 < a2)
+
+        # Сравнение с самолетом без высоты
+        a4 = sample_aeroplanes_objects[3]  # altitude = None
+        assert not (a1 < a4)
+        assert not (a4 < a1)
+
+    def test_aeroplanes_comparison_gt(self, sample_aeroplanes_objects):
+        """Тест сравнения 'больше' по высоте."""
+        a1, a2, a3 = sample_aeroplanes_objects[:3]
+
+        # a1 (10500) > a2 (9500)
+        assert a1 > a2
+        assert not (a2 > a1)
+
+    def test_aeroplanes_comparison_eq(self, sample_aeroplanes_objects):
+        """Тест сравнения на равенство."""
+        a1, a2 = sample_aeroplanes_objects[:2]
+        a1_copy = Aeroplane.from_dict({
+            'callsign': 'AFL101',
+            'origin_country': 'Russia',
+            'velocity': 250.5,
+            'altitude': 10500.0,
+            'latitude': 55.5,
+            'longitude': 37.5,
+            'icao24': 'abc123'
+        })
+
+        assert a1 == a1_copy
+        assert a1 != a2
+        assert a1 != "not an aeroplane"
+
+    def test_to_dict(self, sample_aeroplanes_objects):
+        """Тест преобразования в словарь."""
+        aeroplane = sample_aeroplanes_objects[0]
+        data = aeroplane.to_dict()
+
+        assert data['callsign'] == 'AFL101'
+        assert data['origin_country'] == 'Russia'
+        assert data['velocity'] == 250.5
+        assert data['altitude'] == 10500.0
+        assert data['latitude'] == 55.5
+        assert data['longitude'] == 37.5
+        assert data['icao24'] == 'abc123'
+
+    def test_from_dict(self, sample_aeroplanes_data):
+        """Тест создания из словаря."""
+        data = sample_aeroplanes_data[0]
+        aeroplane = Aeroplane.from_dict(data)
+
+        assert aeroplane.callsign == 'AFL101'
+        assert aeroplane.origin_country == 'Russia'
+
+    def test_cast_to_object_list(self, sample_aeroplanes_data):
+        """Тест преобразования списка словарей в список объектов."""
+        aeroplanes_list = Aeroplane.cast_to_object_list(sample_aeroplanes_data)
+
+        assert len(aeroplanes_list) == 4
+        assert all(isinstance(a, Aeroplane) for a in aeroplanes_list)
+
+    def test_str_representation(self, sample_aeroplanes_objects):
+        """Тест строкового представления."""
+        aeroplane = sample_aeroplanes_objects[0]
+        str_repr = str(aeroplane)
+
+        assert 'AFL101' in str_repr
+        assert 'Russia' in str_repr
+        assert '10500.0' in str_repr
